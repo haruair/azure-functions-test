3d11da60de0e9687e9aec294cd646d5ab06d19ff
Edward Kim <onward.edkim@gmail.com>
2017-07-20 18:44:58 +1000

    Fix str count

--- SpeedTestPHP2/run.php
+++ SpeedTestPHP2/run.php
@@ -3,14 +3,14 @@ ob_start();
 phpinfo(INFO_ENVIRONMENT);
 $info = ob_get_clean();
 $split = "Variable => Value\n";
-$data = substr($info, strpos($info, $split) + count($split));
+$data = substr($info, strpos($info, $split) + strlen($split));
 $data = explode("\n", $data);
 
 array_map(function($row) {
     $split = ' => ';
     $p = strpos($row, $split);
     $key = substr($row, 0, $p);
-    $value = substr($row, $p + count($split));
+    $value = substr($row, $p + strlen($split));
     if (!empty($key)) {
         $_ENV[$key] = $value;
     }
