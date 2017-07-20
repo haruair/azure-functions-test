82596f1b8b172cfff749334da9d13055f3a9a003
Edward Kim <ekim@swin.edu.au>
2017-07-20 14:31:06 +1000

    Add print out on speed test

--- SpeedTestPHP/run.php
+++ SpeedTestPHP/run.php
@@ -1,3 +1,10 @@
 <?php
 fwrite(STDOUT, print_r($_GET) . PHP_EOL);
 fwrite(STDOUT, print_r($_POST) . PHP_EOL);
+
+$data = json_decode(file_get_contents('php://input'));
+
+echo json_encode([
+    'okay' => true,
+    'data' => $data,
+]);
