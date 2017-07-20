6f9ab8a58914e4a9bc7657b1df191cbcd4e3dbe7
Edward Kim <ekim@swin.edu.au>
2017-07-20 14:37:28 +1000

    Fix function

--- SpeedTestPHP/run.php
+++ SpeedTestPHP/run.php
@@ -4,7 +4,7 @@ fwrite(STDOUT, print_r($_POST) . PHP_EOL);
 
 $data = json_decode(file_get_contents('php://input'));
 
-return json_encode([
+$result = json_encode([
     'okay' => true,
     'data' => $data,
     'env' => $_ENV,
