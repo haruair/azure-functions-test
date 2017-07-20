1e77a3fa8dc738715c62934107835cd918e65c97
Edward Kim <ekim@swin.edu.au>
2017-07-20 11:26:02 +1000

    Add base runner

---
+++ AzureFunctions/Runner.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Haruair\AzureFunctions;
+
+class Runner
+{
+    protected $basePath;
+
+    public static function Run(string $basePath)
+    {
+        $runner = new Self($basePath);
+    }
+
+    protected function __construct(string $basePath)
+    {
+        $this->basePath = $basePath;
+        fwrite(STDOUT, $this->getEntryPoint);
+    }
+
+    public function getEntryPoint()
+    {
+        $config = json_decode(file_get_contents(__DIR__.'/function.json'));
+        return $config->entryPoint;
+    }
+}
--- QueueTriggerPHP1/run.php
+++ QueueTriggerPHP1/run.php
@@ -1,4 +1,3 @@
 <?php
-$data = json_decode(file_get_contents(__DIR__.'/function.json'));
-fwrite(STDOUT, print_r($data, true));
-?>
+require_once(__DIR__.'/../bootstrap.php');
+Haruair\AzureFunctions\Runner::run(__DIR__);
