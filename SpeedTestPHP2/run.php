1c42d945e454b6ce681653b34843f48b1a0c4cf0
Edward Kim <onward.edkim@gmail.com>
2017-07-20 18:37:45 +1000

    New line issue

--- SpeedTestPHP2/run.php
+++ SpeedTestPHP2/run.php
@@ -2,7 +2,12 @@
 ob_start();
 phpinfo(INFO_ENVIRONMENT);
 $info = ob_get_clean();
-$data = substr($info, strpos($info, "Variable => Value". PHP_EOL));
-$data = explode(PHP_EOL, $data);
+$data = substr($info, strpos($info, "Variable => Value\n"));
+$data = explode("\n", $data);
+
+print_r($data);
+
+$data = substr($info, strpos($info, "Variable => Value\r\n"));
+$data = explode("\r\n", $data);
 
 print_r($data);
