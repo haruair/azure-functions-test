14d25adfeb31026b05ed123688316ad5ac0041e3
Edward Kim <onward.edkim@gmail.com>
2017-07-20 18:36:32 +1000

    Print out data

--- SpeedTestPHP2/run.php
+++ SpeedTestPHP2/run.php
@@ -2,6 +2,7 @@
 ob_start();
 phpinfo(INFO_ENVIRONMENT);
 $info = ob_get_clean();
-$data = substr($info, strpos($info, "Variable => Value\n"));
-$data = explode("\n", $data);
-echo $data;
+$data = substr($info, strpos($info, "Variable => Value". PHP_EOL));
+$data = explode(PHP_EOL, $data);
+
+print_r($data);
