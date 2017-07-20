c8b0a1308309abe6ad1778db313501e5373b3883
Edward Kim <ekim@swin.edu.au>
2017-07-20 14:33:24 +1000

    Add some global variables

--- SpeedTestPHP/run.php
+++ SpeedTestPHP/run.php
@@ -4,7 +4,9 @@ fwrite(STDOUT, print_r($_POST) . PHP_EOL);
 
 $data = json_decode(file_get_contents('php://input'));
 
-echo json_encode([
+$return = json_encode([
     'okay' => true,
     'data' => $data,
+    'env' => $_ENV,
+    'server' => $_SERVER,
 ]);
