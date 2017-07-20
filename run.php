1f9c111da73de0bdcb65d4633fa56802ec0cdfa2
Edward Kim <ekim@swin.edu.au>
2017-07-20 16:11:43 +1000

    Add comment

--- run.php
+++ run.php
@@ -1,4 +1,9 @@
 <?php
+/**
+ * AzureFuntions for PHP
+ *
+ * This function executes based on `function.json`.
+ */
 require_once(__DIR__.'/bootstrap.php');
 
 new Haruair\AzureFunctions\FunctionApp();
