d4bb81ce5a7d658726ac630a1fa89c19842f02c5
Edward Kim <ekim@swin.edu.au>
2017-07-20 11:41:10 +1000

    Add some message

--- AzureFunctions/Runner.php
+++ AzureFunctions/Runner.php
@@ -8,22 +8,26 @@ class Runner
 
     public static function Run(string $basePath = null)
     {
+        fwrite(STDOUT, 'call Run');
         if (is_null($basePath))
         {
             $basePath = $_SERVER['EXECUTION_CONTEXT_FUNCTIONDIRECTORY'];
         }
+        fwrite(STDOUT, 'basePath is ' . $basePath);
         $runner = new Self($basePath);
     }
 
     protected function __construct(string $basePath)
     {
         $this->basePath = $basePath;
+        fwrite(STDOUT, 'constructed');
         fwrite(STDOUT, $this->getEntryPoint());
     }
 
     public function getEntryPoint()
     {
         $config = json_decode(file_get_contents($this->basePath.'/function.json'));
+        fwrite(STDOUT, 'get Entry Point');
         return $config->entryPoint;
     }
 }
