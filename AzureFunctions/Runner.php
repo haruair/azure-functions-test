0ebc30866a5c4d1386432b8c07aa206a65d767f2
Edward Kim <ekim@swin.edu.au>
2017-07-20 12:18:25 +1000

    Update runner

--- AzureFunctions/Runner.php
+++ AzureFunctions/Runner.php
@@ -4,8 +4,11 @@ namespace Haruair\AzureFunctions;
 
 class Runner
 {
-    public static function Run(string $basePath = null)
+    protected $basePath;
+
+    protected function __construct(string $basePath = null)
     {
+
         fwrite(STDOUT, 'call Run'.PHP_EOL);
         if (is_null($basePath))
         {
@@ -17,21 +20,7 @@ class Runner
             $basePath = __DIR__.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'HelloPHP2';
         }
         fwrite(STDOUT, 'basePath is ' . $basePath.PHP_EOL);
-        try {
-            $runner = new InternalRunner($basePath);
-        } catch(\Exception $e) {
-            fwrite(STDOUT, print_r($e, true) . PHP_EOL);
-        }
-        
-    }
-}
 
-class InternalRunner {
-
-    protected $basePath;
-    
-    protected function __construct(string $basePath)
-    {
         $this->basePath = $basePath;
         fwrite(STDOUT, 'constructed'.PHP_EOL);
         fwrite(STDOUT, $this->getEntryPoint().PHP_EOL);
--- run.php
+++ run.php
@@ -1,3 +1,3 @@
 <?php
 require_once(__DIR__.'/bootstrap.php');
-Haruair\AzureFunctions\Runner::Run();
+new Haruair\AzureFunctions\Runner();
